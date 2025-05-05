dd61c4d19958f19d09538ae35dd8455224636675
userFrontend <aba06096@gmail.com>
2025-05-05 14:52:16 +0500

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      Hello
+      <header>
+        <div className="logo-site"></div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About Us</a>
+              <a href="#">Contact Us</a>
+              <a href="#">(666) 412-2323</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section className='hero'>
+          <h1>MAKING GREAT THINGS HAPPEN WITH
+          <span>BNG CARGO</span></h1>
+          <h4>NEWER TRUCKS</h4>
+          <h4>FAST AND SAFE DELIVERY</h4>
+          <h4>DEDICATED DISPATCHERS</h4>
+          <div className="hero-set">
+            <div className="set-item">
+              <img src="" alt="" />
+              <h3>Delivered More</h3>
+              <b>1,200 +</b>
+              <p>Loads Per Week On Average and Many Deliver More</p>
+            </div>
+            <div className="set-item">
+              <img src="" alt="" />
+              <h3>Earn More</h3>
+              <b>30%</b>
+              <p>~ 30% More than the Industry Average</p>
+            </div>
+            <div className="set-item">
+              <img src="" alt="" />
+              <h3>Live More</h3>
+              <b>95%  </b>
+              <p>BNG Cargo's Home-On-Time Goal for Drivers</p>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
+      <App />
   </>,
 )
